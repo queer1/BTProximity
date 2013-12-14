589e62d1d8030b58aa97b0ca31522a9b4703403e
Alex Bogdan Covaci <alex@azcend.com>
2013-12-14 19:59:07 +0200

    calibration window controller

---
+++ Mac/BTProximity/BPCalibrationWindowController.h
@@ -0,0 +1,13 @@
+//
+//  BPCalibrationWindowController.h
+//  BTProximity
+//
+//  Created by Bogdan Covaci on 14.12.2013.
+//  Copyright (c) 2013 Alex Covaci. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BPCalibrationWindowController : NSWindowController
+
+@end
---
+++ Mac/BTProximity/BPCalibrationWindowController.m
@@ -0,0 +1,33 @@
+//
+//  BPCalibrationWindowController.m
+//  BTProximity
+//
+//  Created by Bogdan Covaci on 14.12.2013.
+//  Copyright (c) 2013 Alex Covaci. All rights reserved.
+//
+
+#import "BPCalibrationWindowController.h"
+
+@interface BPCalibrationWindowController ()
+
+@end
+
+@implementation BPCalibrationWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
